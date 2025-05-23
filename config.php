b8add9d046fdcfb4ec40fcff92eb8d8208b818d7
Ossi Weck <ossi.weck@student.careeria.fi>
2025-05-23 17:46:53 +0300

    modattu configia

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $serverName = getenv("DB_SERVER");
 $database = getenv("DB_NAME");
 $username = getenv("DB_USER");
@@ -9,5 +9,25 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Yhteysvirhe: " . $e->getMessage());
+}
+?> -->
+
+<?php
+$serverName = getenv("DB_SERVER");
+$database = getenv("DB_NAME");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+
+echo "Trying to connect to server: $serverName<br>";
+echo "Database: $database<br>";
+echo "Username: $username<br>";
+// Älä tulosta salasanaa
+
+try {
+    $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Yhteys tietokantaan onnistui!";
+} catch (PDOException $e) {
+    die("Tietokantavirhe: " . $e->getMessage());
 }
 ?>
